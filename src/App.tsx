bc5090a4ae4c2fa8bd0f8641025b509dc563426b
TeamLevant01 <teamlbx01@levantbizexperts.com>
2024-06-14 16:29:39 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { dummyData } from "./data/todos";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [todos, setTodos] = useState(dummyData);
+
+  function setTodoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
+    );
+  }
+
+  function addTodo(title: string) {
+    setTodos((prevTodos) => [
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed: false,
+      },
+      ...prevTodos,
+    ]);
+  }
+
+  function deleteTodo(id: number) {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  }
+
+  return (
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
+      <h1 className="font-bold text-3xl text-center">Shehana's to-do list</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm onSubmit={addTodo} />
+        <TodoList
+          todos={todos}
+          onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo}
+        />
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/data/todos.ts
@@ -0,0 +1,11 @@
+import { Todo } from "../types/todo";
+
+
+export const dummyData: Todo[] = [
+
+    {
+        id:1,
+        title: "Shehana ඔබ හරිම හුරුබුහුටියි 😉",
+        completed:false,
+    },
+];
