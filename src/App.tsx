2fb02414713955e21cf467fdfa22fb1c5e41ac54
TeamLevant01 <teamlbx01@levantbizexperts.com>
2024-06-15 09:49:51 +0530

    2 commit

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <main className="py-10 h-screen space-y-5 overflow-y-auto">
-      <h1 className="font-bold text-3xl text-center">Shehana's to-do list</h1>
+      <h1 className="font-bold text-3xl text-center">My Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
         <AddTodoForm onSubmit={addTodo} />
         <TodoList
--- src/data/todos.ts
+++ src/data/todos.ts
@@ -5,7 +5,7 @@ export const dummyData: Todo[] = [
 
     {
         id:1,
-        title: "Shehana ඔබ හරිම හුරුබුහුටියි 😉",
+        title: "Hello 😉",
         completed:false,
     },
 ];
